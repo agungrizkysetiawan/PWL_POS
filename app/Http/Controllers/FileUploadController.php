b22624375a054f5375e89aacd522da8ed062bcf5
agungrizkysetiawan <agungrizkysetiawannn@gmail.com>
2024-06-08 12:56:00 +0700

    JB 12 - PRAK F

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -17,9 +17,11 @@ class FileUploadController extends Controller
         // $path = $request->berkas->store('uploads');
         $extfile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-'.time().".".$extfile;
-        $path = $request->berkas->storeAs('public', $namaFile);
+        $path = $request->berkas->move('gambar',$namaFile);
+        $path = str_replace("\\","//", $path);
+        echo "Variabel path berisi:$path <br>";
 
-        $pathBaru = asset('storage/'.$namaFile);
+        $pathBaru = asset('gambar/'.$namaFile);
         echo "proses upload berhasil, data disimpan pada:$path";
         echo "<br>";
         echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
